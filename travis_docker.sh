b79ac6fa3590ce2a456e133539285ca84c4b1f5a
Jonah Brüchert <jbb.prv@gmx.de>
2018-09-24 18:46:53 +0200

    Build docker images on travis ci

---
+++ travis_docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+build() {
+	for image in *; do
+		if [ -f $image/Dockerfile ]; then
+			cd $image
+			docker build -t "kaidan/${image}:latest" .
+			cd ..
+		fi
+	done
+}
+
+push() {
+	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+	for image in *; do
+		if [ -f $image/Dockerfile ]; then
+			docker docker push -t "kaidan/${image}:latest"
+		fi
+	done
+}
+
+$1
